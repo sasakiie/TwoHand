7799391c4c6724106cfc97b361389444d07fc82f
Phawothai Na pairee <bright4869napairee@gmail.com>
2024-12-03 15:12:13 +0700

    delete testing rewrite to authen

--- src/routes/Authentication/user-register.svelte
+++ src/routes/Authentication/user-register.svelte
@@ -1,67 +1,51 @@
-
 <svelte:head>
   <title>2hand E-commerce platform</title>
   <!-- You can add other meta tags or head elements here -->
   <meta name="description" content="A platform for buying and selling second-hand items.">
   <meta charset="UTF-8">
-  
 </svelte:head>
-<div class="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
-    <h1 class="text-3xl font-bold text-white">Welcome to</h1> <br>
-    <img src="/logo.png" alt="" class=" w-[274px] ">
-    <form class="bg-[#058CD7] px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-            <label for="First_Name" class="block text-white text-sm font-bold mb-2">
-                First Name
-            </label>
-            <input class="shadow border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
-            type="text" 
-            name="First_Name"
-            id="First_Name" />
+
+<div class="min-h-screen py-48 bg-gradient-to-r from-sky-500 to-indigo-500 ">
+    <div class="container mx-auto ">
+        <div class="flex flex-col lg:flex-row w-10/12 lg:w-8/12 bg-white rounded-xl mx-auto shadow-lg overflow-hidden  ">
+            <div class="w-full lg:w-1/2 flex flex-col items-center justify-center p-12 bg-no-repeat bg-cover bg-center bg-[#058CD7]">
+                <img src="/logo.png" alt="" class=" w-[274px] ">                
+            </div>
+            <div class="w-full lg:w-1/2 py-16 px-12">
+                <h2 class="text-3xl mb-4">Register</h2>
+                <p class="mb-4">
+                    &nbsp;&nbsp;&nbsp;Create your account.
+                </p>
+                <form action="#">
+                    <div class="grid grid-cols-2 gap-5">
+                        <input type="text" placeholder="FirstName" class="shadow border rounded-full py-1 px-2 text-gray-700 leading-tight focus:outline-none">
+                        <input type="text" placeholder="SurName" class="shadow border rounded-full py-1 px-2 text-gray-700 leading-tight focus:outline-none">
+                    </div>
+                    <div class="mt-5">
+                        <input type="email" placeholder="Email" class=" w-full shadow border rounded-full py-1 px-2 text-gray-700 leading-tight focus:outline-none">
+                    </div>
+                    <div class="mt-5">
+                        <input type="password" placeholder="Password" class=" w-full shadow border rounded-full py-1 px-2 text-gray-700 leading-tight focus:outline-none">
+                    </div>
+                    <div class="mt-5">
+                        <input type="password" placeholder="Confirm Password" class=" w-full shadow border rounded-full py-1 px-2 text-gray-700 leading-tight focus:outline-none">
+                    </div>
+                    <div class="mt-5">
+                        <button 
+                        type="submit"
+                        class="bg-red-500 hover:bg-red-700 
+                        text-white font-bold py-2 px-4 rounded-full focus:outline-none w-full">
+                        REGISTER
+                    </button>
+                    </div>
+                    <div class="mt-5 text-center">
+                        <p class="text-gray-600 text-sm">
+                          Already have an account? 
+                          <a href="/login" class="text-blue-500 hover:underline">Log in</a>
+                        </p>
+                      </div>
+                    </form>
+            </div>
         </div>
-        <div class="mb-4">
-            <label for="Last_Name" class="block text-gray-700 text-sm font-bold mb-2">
-                Last Name
-            </label>
-            <input class="shadow border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
-            type="text" 
-            name="Last_Name"
-            id="Last_Name" />
-        </div>
-        <div class="mb-4">
-            <label for="email" class="block text-gray-700 text-sm font-bold mb-2">
-                Email
-            </label>
-            <input class="shadow border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
-            type="email" 
-            name="email"
-            id="email" />
-        </div>
-        <div class="mb-4">
-            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">
-                Password
-            </label>
-            <input class="shadow border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
-            type="password" 
-            name="password"
-            id="password" />
-        </div>
-        <div class="mb-4">
-            <label for="password_confirm" class="block text-gray-700 text-sm font-bold mb-2">
-                Confirm Password
-            </label>
-            <input class="shadow border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" 
-            type="password" 
-            name="password_confirm"
-            id="password_confirm" />
-        </div> <br>
-        <div class="flex items-center">
-            <button 
-                type="submit"
-                class="bg-red-500 hover:bg-red-700 
-                text-white font-bold py-2 px-4 rounded-full focus:outline-none w-full">
-                REGISTER
-            </button>
-        </div>
-    </form>
+    </div>
 </div>
